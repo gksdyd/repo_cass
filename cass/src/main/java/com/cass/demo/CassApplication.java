28dc06c6af53cb3546ae55fedc51d68dbb9f31de
gksdyd <gksdyd009@naver.com>
2025-04-07 14:11:08 +0900

    1. git Init

---
+++ cass/src/main/java/com/cass/demo/CassApplication.java
@@ -0,0 +1,13 @@
+package com.cass.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassApplication.class, args);
+	}
+
+}
---
+++ cass/src/test/java/com/cass/demo/CassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cass.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
