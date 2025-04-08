f077fc5a68777595c31a89d14ddd7abe588c1ab4
gksdyd <gksdyd009@naver.com>
2025-04-08 12:51:44 +0900

    1. 입고 관리 페이지 기본 세팅 완료

---
+++ cass/src/main/java/com/cass/demo/mobile/service/IncomeController.java
@@ -0,0 +1,14 @@
+package com.cass.demo.mobile.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/mobileXdm/service")
+public class IncomeController {
+
+	@RequestMapping(value = "/IncomeXdmList")
+	public String incomeXdmForm() {
+		return "mobileXdm/service/IncomeXdmList";
+	}
+}
