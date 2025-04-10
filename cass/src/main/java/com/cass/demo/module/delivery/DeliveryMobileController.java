229c2d42cf11397b47497422bda974d9dc8b9801
gksdyd <gksdyd009@naver.com>
2025-04-10 20:19:59 +0900

    1. 출고 리스트 함수 중복으로 인한 오류 수정

--- cass/src/main/java/com/cass/demo/module/delivery/DeliveryMobileController.java
+++ cass/src/main/java/com/cass/demo/module/delivery/DeliveryMobileController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cass.demo.base.xdm.BaseController;
-import com.cass.demo.module.parts.PartsVo;
 
 
 
@@ -25,20 +24,4 @@ public class DeliveryMobileController extends BaseController {
 		model.addAttribute("list", deliveryService.selectList(vo));
 		return "mobileXdm/delivery/DeliveryMobileXdmList";
 	}
-	
-	@RequestMapping(value = "/DeliveryXdmList")
-	public String partXdmList(Model model, @ModelAttribute("vo") DeliveryVo vo) {
-		utildatetime(vo);
-		
-		vo.setParamsPaging(deliveryService.selectOneCount(vo));
-
-		if (vo.getTotalRows() > 0) {
-			model.addAttribute("list", deliveryService.selectList(vo));
-		}
-
-		model.addAttribute("vo", vo);
-		
-		return "xdm/delivery/DeliveryXdmList";
-	}
-
 }
