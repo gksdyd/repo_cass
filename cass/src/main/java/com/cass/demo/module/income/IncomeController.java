b367dd14ac1bca61fc0e98268104857088d97707
gksdyd <gksdyd009@naver.com>
2025-04-10 16:50:45 +0900

    1. 입고 페이지 pagination, search 완료

--- cass/src/main/java/com/cass/demo/module/income/IncomeController.java
+++ cass/src/main/java/com/cass/demo/module/income/IncomeController.java
@@ -3,6 +3,7 @@ package com.cass.demo.module.income;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cass.demo.base.xdm.BaseController;
@@ -15,17 +16,10 @@ public class IncomeController extends BaseController {
 	IncomeService service;
 	
 	@RequestMapping(value = "/IncomeXdmList")
-	public String incomeXdmForm(IncomeVo vo, Model model) {
-		
+	public String incomeXdmForm(@ModelAttribute("vo") IncomeVo vo, Model model) {
 		utildatetime(vo);
-		
 		vo.setParamsPaging(service.selectOneCount(vo));
-		
 		model.addAttribute("list", service.selectList(vo));
-		model.addAttribute("vo", vo);
-		
-//		model.addAttribute("list", service.selectList(dto));
-		
 		return "mobileXdm/income/IncomeXdmList";
 	}
 	
